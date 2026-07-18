f455d299bb9974f52272e18f2503f7c8d2699590
Dilan36 <70045728+Dilan36@users.noreply.github.com>
2026-07-17 22:08:45 -0500

    Update API_URL to the new Google Apps Script URL

--- mi-entrenamiento/js/config.js
+++ mi-entrenamiento/js/config.js
@@ -1,4 +1,4 @@
 // Pega aquí la URL de tu Web App de Google Apps Script
 // (la obtienes al hacer "Implementar" > "Nueva implementación" en el editor de Apps Script)
 // Ejemplo: "https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxxxxxxxx/exec"
-const API_URL = "PEGA_AQUI_TU_URL_DE_APPS_SCRIPT";
+const API_URL = "https://script.google.com/macros/s/AKfycbyIry18zb-X7i-UTN3TR3Y0ACWBEe_3F9qvusfAOuac0ihDIvnc8MBu6jNDT45DnDA2OA/exec";
