cbd31c48cf22977e7dc2eec2b294f05d50ae6c6f
Dilan36 <70045728+Dilan36@users.noreply.github.com>
2026-07-17 22:01:57 -0500

    Add files via upload

---
+++ mi-entrenamiento/js/config.js
@@ -0,0 +1,4 @@
+// Pega aquí la URL de tu Web App de Google Apps Script
+// (la obtienes al hacer "Implementar" > "Nueva implementación" en el editor de Apps Script)
+// Ejemplo: "https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxxxxxxxx/exec"
+const API_URL = "PEGA_AQUI_TU_URL_DE_APPS_SCRIPT";
